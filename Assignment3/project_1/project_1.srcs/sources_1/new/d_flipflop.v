754a882b019cf9de9733730f00bba14412910849
AnanthMSIS <113513284+AnanthMSIS@users.noreply.github.com>
2022-11-24 18:40:21 +0530

    Add files via upload

---
+++ Assignment3/project_1/project_1.srcs/sources_1/new/d_flipflop.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.11.2022 18:15:14
+// Design Name: 
+// Module Name: d_flipflop
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module d_flipflop(
+    input d,
+    input clk,
+    input reset,
+    output reg q,
+    output reg qb
+    );
+    
+    always @ (posedge clk or reset)
+    begin
+        if(!reset)
+          assign q = d;
+        else
+          assign q = 1'b0;
+        assign qb = ~q;       
+    end
+endmodule
