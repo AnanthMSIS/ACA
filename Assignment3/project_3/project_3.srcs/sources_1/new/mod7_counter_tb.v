51895de33c63ad3cf59e2bd948b38b21e2e7b002
AnanthMSIS <113513284+AnanthMSIS@users.noreply.github.com>
2022-11-28 17:54:15 +0530

    Add files via upload

---
+++ Assignment3/project_3/project_3.srcs/sources_1/new/mod7_counter_tb.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.11.2022 11:57:27
+// Design Name: 
+// Module Name: mod7_counter_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mod7_counter_tb();
+    reg clk, reset;
+    wire [2:0]f;
+    
+    mod7_counter m1 (clk, reset, f);
+    
+    initial 
+    begin
+        clk = 0;
+        forever #10 clk = ~clk;
+    end
+    
+    initial
+    begin
+        reset = 1'b0;
+        #40 reset = 1'b1;
+        #50 reset = 1'b0;
+        #20 reset = 1'b1;
+        #5 reset = 1'b0;
+    end
+endmodule
